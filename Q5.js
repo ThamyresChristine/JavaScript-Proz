1d232c74218939c0922c4ac3a50e7b7df4919647
Thamyres de Andrade Soares <102846350+ThamyresChristine@users.noreply.github.com>
2024-12-09 23:57:09 -0300

    Update Q5.js

--- Q5.js
+++ Q5.js
@@ -1,72 +1,16 @@
-﻿var bot = Math.ceil(Math.random(0,2))
-/*
-0 Pedra
-1 Papel 
-2 Tesoura
-*/
-var hum = prompt("Escolha: Pedra, Papel ou Tesoura")
-hum = hum.toLowerCase();
+var filme ="Vingadores - Guerra Infinita"
 
-var pontosBot = 0
-var pontosVc = 0
+//tamanho - quantidade de caracteres
+document.write("<br>A quantidade de caracteres é : " + filme.length)
 
-if (hum == "pedra") {
-    if (bot == 0) {
-        document.write("empate") 
-    } 
-    if (bot == 1) {
-        document.write("bot ganhou")
-        pontosBot = pontosBot + 1; 
-        
-    }
-    if (bot == 2) {
-       document.write("vc ganhou") 
-       pontosVc = pontosVc + 1
-       
-    }
+//letra minúscula
+document.write("<br>Texto em letras maiúsculas: " + filme.toLowerCase())
 
-} else if (hum == "papel") {
-    if (bot == 0) {
-        document.write("vc ganhou") 
-        pontosVc = pontosVc + 1
-        
-    } 
-    if (bot == 1) {
-        document.write("empate") 
-    }
-    if (bot == 2) {
-       document.write("bot ganhou") 
-       pontosBot = pontosBot + 1;
-       
-    }
-} else {
-    if (bot == 0) {
-        document.write("bot ganhou") 
-        pontosBot = pontosBot + 1;
-        
-    } 
-    if (bot == 1) {
-        document.write("vc ganhou") 
-        pontosVc = pontosVc + 1
-    }
-    if (bot == 2) {
-       document.write("empate") 
-    }
-}
+//letra maiúscula
+document.write("<br>Texto em letras minúscúsculas: " + filme.toUpperCase())
 
-document.write("<br> Escolha do bot " + bot)
-document.write("<br> Sua escolha " + hum)
+//substring
+document.write("<br>Texto cortado: " + filme.substring(12, 20))
 
-document.write ("<br>Seu Total de pontos atualizados: " + pontosVc)
-document.write ("<br>Total de pontos atualizados do Bot: " + pontosBot)
-
-
-
-
-if (pontosBot == 10){ 
-    document.write ("<br>Total de pontos atualizados do Bot: " + pontosBot + "Bot ganhou")
-
-} else if (pontosVc == 10) {
-    document.write ("<br>Seu Total de pontos atualizados: " + pontosVc + "Vc ganhou")
-
-}
+//index da letra G
+document.write("<br>index da letra G: " + filme.indexOf("G"))
