e25b9c91f3ecb57719e12010a6f0548819b05d3c
Thamyres de Andrade Soares <102846350+ThamyresChristine@users.noreply.github.com>
2024-12-09 23:56:43 -0300

    Update Q4.js

--- Q4.js
+++ Q4.js
@@ -1,30 +1,3 @@
-﻿var nome = prompt("Olá usuário! Por favor, digite seu nome:");
-var altura = prompt("Olá " + nome + " Agora digite sua altura Em Centímetros:");
-var peso = prompt("E por fim, digite seu peso:")
-        
-        altura = parseFloat(altura)
-        peso = parseFloat(peso);
+var calculo =  (12 * 3) + 4 - (8 / 2) % 3
 
-        altura = altura/100;
-
-        Imc = (peso / (altura**=2));    
-    
-        if (Imc < 16.00){
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ", sendo classificado como 'Peso baixo - muito grave'.");
-        } else if (Imc > 16.00 && Imc < 16.99) {
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Peso baixo - grave'.");
-        } else if (Imc > 17.00 && Imc < 18.49) {
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Peso baixo'.");
-        } else if (Imc > 18.50 && Imc < 24.99) {
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Peso normal'.");
-        } else if (Imc > 25.00 && Imc < 29.99) {
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Sobrepeso'.");
-        } else if (Imc > 30.00 && Imc < 34.99) {    
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Obesidade grau I'.");
-        } else if (Imc > 35.00 && Imc < 39.99) {    
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Obesidade grau II'.");
-        } else if (Imc > 40.00) {    
-            document.write("<br> " + nome + ", você possui o IMC = " + Imc + ",  sendo classificado como 'Obesidade grau III'.");    
-        } else {    
-            document.write("Dados incompletos ou inconsistentes");
-        } 
+document.write(" O resultado da expressão 12 * 3 + 4 - 8 / 2 % 3 é: " + calculo)
