fb06c6ece7103cbaf9b429a76d5aad7f6500c739
Thamyres de Andrade Soares <102846350+ThamyresChristine@users.noreply.github.com>
2024-12-09 23:54:57 -0300

    Delete Q1.js
    del q1

--- Q1.js
+++
@@ -1,9 +0,0 @@
-﻿var numUm = prompt("Digite um número: ")
-
-var numDois = prompt("Digite outro número: ")
-
-if (numUm == numDois) {
-    document.write("Os números são iguais.")
-} else {
-    document.write("Os números são diferentes.")
-}
